a46a9bdef306948fdc138400e4635257b560ca26
Joakim Sjöstedt <joakim.sjostedt@softhouse.se>
2020-03-23 08:40:27 +0100

    Initial Commit

---
+++ DependencyInjectionProtocolExtension/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DependencyInjectionProtocolExtension
+//
+//  Created by Joakim Sjöstedt on 2020-03-23.
+//  Copyright © 2020 Joakim Sjöstedt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
