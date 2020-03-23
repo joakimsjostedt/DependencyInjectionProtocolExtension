50a29f9f27a13af8bd1da9dd25636920208db6b2
Joakim Sjöstedt <joakim.sjostedt@softhouse.se>
2020-03-23 08:43:14 +0100

    First Commit

--- DependencyInjectionProtocolExtension/ViewController.swift
+++ DependencyInjectionProtocolExtension/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  DependencyInjectionProtocolExtension
+//  testillitest
 //
 //  Created by Joakim Sjöstedt on 2020-03-23.
 //  Copyright © 2020 Joakim Sjöstedt. All rights reserved.
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class BaseClass {
+    
+    var myString: String = "All the way from the back!"
+}
+
+class InheritingClass: BaseClass {
+    
+    func inheritingFuntion() {
+        print(myString)
+    }
+}
+
+protocol InjectionProtocol {
+    var instance: InheritingClass { get }
+}
+
+extension InjectionProtocol {
+    var instance: InheritingClass {
+        return InheritingClass()
+    }
+}
+
+class ViewController: UIViewController, InjectionProtocol {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        instance.inheritingFuntion()
+
     }
-
-
 }
 
